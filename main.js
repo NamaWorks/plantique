9788cdeb59616efb3236fef1c859d6ee10f8d38d
NamaWorks <pedronayamartinez@hotmail.com>
2023-11-17 20:46:09 +0100

    add new minor fixes to V02

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const products = [
     imageUrl:
       'url(https://res.cloudinary.com/dgrhbsilh/image/upload/v1699870730/Plantique/opt/golden_pothos_wljaai_qgjpsu.jpg)',
     description:
-      'This spiky, low-maintenance houseplant was the top-searched plant in nearly a dozen states, according to a recent survey by home furnishings site Joybird.',
+      'The Golden Pothos plant, also known as Epipremnum aureum, is a popular houseplant characterized by its heart-shaped leaves with vibrant golden-yellow variegation. It is a low-maintenance and versatile plant that thrives in various light conditions, making it an ideal choice for both beginner and experienced plant enthusiasts.',
     productLink: 'https://www.google.com/'
   },
   {
@@ -27,7 +27,7 @@ const products = [
     imageUrl:
       'url(https://res.cloudinary.com/dgrhbsilh/image/upload/v1699870740/Plantique/opt/bamboo_dyxygv_ck9dqa.jpg)',
     description:
-      'This spiky, low-maintenance houseplant was the top-searched plant in nearly a dozen states, according to a recent survey by home furnishings site Joybird.',
+      'The bamboo plant is a highly adaptable and fast-growing perennial grass that is known for its tall, sturdy stalks and feathery green leaves. It is prized for its versatility, being used for construction, furniture, and as a decorative plant in gardens.',
     productLink: 'https://www.google.com/'
   },
   {
@@ -37,7 +37,7 @@ const products = [
     imageUrl:
       'url(https://res.cloudinary.com/dgrhbsilh/image/upload/v1699870734/Plantique/opt/Philodendrons_Scandens_qfvyjp_iciwcc.jpg)',
     description:
-      'This spiky, low-maintenance houseplant was the top-searched plant in nearly a dozen states, according to a recent survey by home furnishings site Joybird.',
+      'The Philodendron scandens, also known as the Heartleaf Philodendron, is a popular trailing houseplant with heart-shaped, glossy green leaves. It is a low-maintenance plant that thrives in various light conditions, making it a perfect choice for adding a touch of greenery to any indoor space.',
     productLink: 'https://www.google.com/'
   },
   {
@@ -47,7 +47,7 @@ const products = [
     imageUrl:
       'url(https://res.cloudinary.com/dgrhbsilh/image/upload/v1699870728/Plantique/opt/croton_f2dstn_kmudds.jpg)',
     description:
-      'This spiky, low-maintenance houseplant was the top-searched plant in nearly a dozen states, according to a recent survey by home furnishings site Joybird.',
+      'The Croton plant, scientifically known as Codiaeum variegatum, is a vibrant and eye-catching houseplant with colorful, variegated leaves in shades of red, orange, yellow, and green. It adds a bold and tropical touch to any indoor space and requires bright light and regular watering to thrive.',
     productLink: 'https://www.google.com/'
   },
   {
@@ -57,87 +57,87 @@ const products = [
     imageUrl:
       'url(https://res.cloudinary.com/dgrhbsilh/image/upload/v1699870741/Plantique/opt/Banyan_Ficus_jsunpi_jxome7.jpg)',
     description:
-      'This spiky, low-maintenance houseplant was the top-searched plant in nearly a dozen states, according to a recent survey by home furnishings site Joybird.',
+      'The Banyan Ficus, scientifically known as Ficus benghalensis, is a magnificent tree with a sprawling canopy and aerial roots that create a captivating and unique appearance. Its grandeur and resilience make it a symbol of strength and longevity, making it a popular choice for landscaping and as an indoor statement plant.',
     productLink: 'https://www.google.com/'
   },
   {
     name: 'Monstera',
-    price: 150,
+    price: 50,
     category: 'outdoor',
     imageUrl:
       'url(https://res.cloudinary.com/dgrhbsilh/image/upload/v1699870733/Plantique/opt/monstera_opsosc_qovml1.jpg)',
     description:
-      'This spiky, low-maintenance houseplant was the top-searched plant in nearly a dozen states, according to a recent survey by home furnishings site Joybird.',
+      'The Monstera, scientifically known as Monstera deliciosa, is a striking and iconic houseplant with large, glossy, and fenestrated leaves that create a tropical and dramatic aesthetic. Its unique leaf shape and ability to thrive in various light conditions make it a popular choice for adding a touch of exotic beauty to any indoor space.',
     productLink: 'https://www.google.com/'
   },
   {
     name: 'Cyclamen',
-    price: 150,
+    price: 96,
     category: 'outdoor',
     imageUrl:
       'url(https://res.cloudinary.com/dgrhbsilh/image/upload/v1699870729/Plantique/opt/Cyclamen_c9gxpu_yikjsv.jpg)',
     description:
-      'This spiky, low-maintenance houseplant was the top-searched plant in nearly a dozen states, according to a recent survey by home furnishings site Joybird.',
+      'The Cyclamen plant, known for its elegant and delicate blooms, is a charming addition to any indoor or outdoor space. With its heart-shaped leaves and vibrant flowers in shades of pink, white, or purple, it brings a touch of beauty and grace to any setting.',
     productLink: 'https://www.google.com/'
   },
   {
     name: 'bougainvillea',
-    price: 150,
+    price: 32,
     category: 'climbing plants',
     imageUrl:
       'url(https://res.cloudinary.com/dgrhbsilh/image/upload/v1699870742/Plantique/opt/bougainvillea_x0wyoz_drqnl1.jpg)',
     description:
-      'This spiky, low-maintenance houseplant was the top-searched plant in nearly a dozen states, according to a recent survey by home furnishings site Joybird.',
+      'The Bougainvillea is a vibrant and showy flowering plant known for its stunning clusters of colorful bracts that surround small, inconspicuous flowers. With its ability to thrive in warm climates and its ability to climb or trail, it adds a burst of beauty and charm to gardens, fences, and trellises.',
     productLink: 'https://www.google.com/'
   },
   {
     name: 'chinese jasmine',
-    price: 150,
+    price: 209,
     category: 'climbing plants',
     imageUrl:
       'url(https://res.cloudinary.com/dgrhbsilh/image/upload/v1699870728/Plantique/opt/chinese_jasmine_w76lkf_kjcsz0.jpg)',
     description:
-      'This spiky, low-maintenance houseplant was the top-searched plant in nearly a dozen states, according to a recent survey by home furnishings site Joybird.',
+      'The Chinese Jasmine, scientifically known as Jasminum polyanthum, is a fragrant and beautiful vine with clusters of delicate white flowers that bloom profusely. Its intoxicating scent and graceful appearance make it a popular choice for gardens and indoor spaces, adding a touch of elegance and serenity.',
     productLink: 'https://www.google.com/'
   },
   {
     name: 'honeysuckle',
-    price: 150,
+    price: 106,
     category: 'climbing plants',
     imageUrl:
       'url(https://res.cloudinary.com/dgrhbsilh/image/upload/v1699870731/Plantique/opt/honeysuckle_zqfvk7_kyjgnb.jpg)',
     description:
-      'This spiky, low-maintenance houseplant was the top-searched plant in nearly a dozen states, according to a recent survey by home furnishings site Joybird.',
+      'Honeysuckle, a fragrant flowering vine, delights with its sweet-scented blossoms and vibrant colors, attracting hummingbirds and butterflies to any garden. Its twining nature and easy maintenance make it a charming addition to trellises, fences, or as a ground cover.',
     productLink: 'https://www.google.com/'
   },
   {
     name: 'mangifera indica',
-    price: 300,
+    price: 189,
     category: 'fruit trees',
     imageUrl:
       'url(https://res.cloudinary.com/dgrhbsilh/image/upload/v1699870733/Plantique/opt/mangifera_indica_gdygjf_muicdu.jpg)',
     description:
-      'This spiky, low-maintenance houseplant was the top-searched plant in nearly a dozen states, according to a recent survey by home furnishings site Joybird.',
+      'Mangifera indica, commonly known as the Mango tree, is a tropical fruit tree renowned for its delicious and juicy mango fruits. With its lush foliage and sweet rewards, it is a prized addition to gardens and orchards, bringing a taste of the tropics to any environment.',
     productLink: 'https://www.google.com/'
   },
   {
     name: 'fig',
-    price: 70,
+    price: 23,
     category: 'fruit trees',
     imageUrl:
       'url(https://res.cloudinary.com/dgrhbsilh/image/upload/v1699870729/Plantique/opt/fig_bmxnro_szlyy4.jpg)',
     description:
-      'This spiky, low-maintenance houseplant was the top-searched plant in nearly a dozen states, according to a recent survey by home furnishings site Joybird.',
+      'The fig plant is a small to medium-sized tree known for its delicious and nutritious fruit. It has large, lobed leaves and a distinct trunk, adding beauty to any garden or landscape.',
     productLink: 'https://www.google.com/'
   },
   {
     name: 'pomegranate',
-    price: 250,
+    price: 289,
     category: 'fruit trees',
     imageUrl:
       'url(https://res.cloudinary.com/dgrhbsilh/image/upload/v1699870736/Plantique/opt/pomegranate_yww32e_itg0qp.jpg)',
     description:
-      'This spiky, low-maintenance houseplant was the top-searched plant in nearly a dozen states, according to a recent survey by home furnishings site Joybird.',
+      'The pomegranate is a vibrant and delicious fruit known for its sweet and tangy flavor, adorned with ruby-colored arils that burst with antioxidants and nutrients. It is often enjoyed fresh or used in a variety of culinary creations, adding a burst of flavor and a pop of color.q',
     productLink: 'https://www.google.com/'
   }
 ]
